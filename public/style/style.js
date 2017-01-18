c30f3fa6042d549050c770df80b8579a0c7aa8a7
xrey007 <reinaldo.leal001@gmail.com>
2017-01-17 19:12:51 -0500

    small changes made to js file

--- public/style/style.js
+++ public/style/style.js
@@ -29,62 +29,67 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
 
-$("#services").click(function(e) {
-  e.preventDefault();
-  $('html,body').animate({
-    scrollTop: $("#services-div").offset().top},
+  $("#services").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("#services-div").offset().top
+    },
     1500)
   });
   $( "#services-btn").click(function(e) {
     e.preventDefault();
     $('html,body').animate({
-      scrollTop: $("#services-div").offset().top},
-      1500)
-    });
-      $("#contact-us").click(function(e) {
-        e.preventDefault();
-        $('html,body').animate({
-          scrollTop: $("#contact-us-div").offset().top},
-          1500)
-        });
-        $( "#services-btn-dd").click(function(e) {
-          e.preventDefault();
-          $('html,body').animate({
-            scrollTop: $("#services-div").offset().top},
-            1500)
-          });
-            $("#contact-us-dd").click(function(e) {
-              e.preventDefault();
-              $('html,body').animate({
-                scrollTop: $("#contact-us-div").offset().top},
-                1500)
-              });
-              $(".logo-button").click(function(e) {
-                e.preventDefault();
-                $('html,body').animate({
-                  scrollTop: $("header").offset().top},
-                  1500)
-                });
+      scrollTop: $("#services-div").offset().top
+    },
+    1500)
+  });
+  $("#contact-us").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("#contact-us-div").offset().top
+    },
+    1500)
+  });
+  $( "#services-btn-dd").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("#services-div").offset().top
+    },
+    1500)
+  });
+  $("#contact-us-dd").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("#contact-us-div").offset().top
+    },
+    1500)
+  });
+  $(".logo-button").click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: $("header").offset().top
+    },
+    1500)
+  });
 
-                var product = $('.our-services').offset().top;
-                var application = $('.applications').offset().top;
-                var contact = $('.contact-us').offset().top;
+  var product = $('.our-services').offset().top;
+  var application = $('.applications').offset().top;
+  var contact = $('.contact-us').offset().top;
 
-                $(window).on('scroll',function(){
-                  var stop = Math.round($(window).scrollTop());
-                  if (stop > product) {
-                    $('.product-li').addClass('active');
-                  } else {
-                    $('.product-li').removeClass('active');
-                  }
-
-
-
-                  if (stop > ((contact - product + 280))) {
-                    $('.product-li').removeClass('active');
-                    $('.contact-li').addClass('active');
-                  } else {
-                    $('.contact-li').removeClass('active');
-                  }
-                });
+  $(window).on('scroll',function(){
+    var stop = Math.round($(window).scrollTop());
+    if (stop > (product - 10)) {
+      $('.product-li').addClass('active');
+    } else {
+      $('.product-li').removeClass('active');
+    };
+    if (stop > ((contact - product + 280))) {
+      $('.product-li').removeClass('active');
+      $('.contact-li').addClass('active');
+    } else {
+      $('.contact-li').removeClass('active');
+    };
+  });
+});
